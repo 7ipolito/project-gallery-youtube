823b350fa962d4df57304cc5ea360730e33b1116
Allan Hipólito <allanhipolito66@gmail.com>
2024-06-25 15:08:28 -0300

    tsyringue to inverse dependency added

--- backend/src/controllers/videoController.ts
+++ backend/src/controllers/videoController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { container } from 'tsyringe';
+import { VideoService } from '../services/videoService';
 
-const prisma = new PrismaClient();
+const userService = container.resolve(VideoService);
 
 export const getAllVideos= async (req: Request, res: Response): Promise<void> => {
   try {
-    const videos = await prisma.video.findMany();
+    const videos = await userService.getAllVideos()
     res.json(videos);
   } catch (error) {
     res.status(500).json({ error: 'Something went wrong' });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+import './container';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';

3a3308718ad6e51041b1221135defba57262ec4a
Allan Hipólito <allanhipolito66@gmail.com>
2024-07-08 01:32:24 -0300

    Merge pull request

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/`,
-=======
-  baseURL: 'http://localhost:3000/api/',
->>>>>>> main
 });

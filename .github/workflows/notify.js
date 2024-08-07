4c658815cfea523903f78399d02e68a3c2f3fd5d
Allan Hipólito <allanhipolito66@gmail.com>
2024-08-07 05:58:25 -0300

    teste

--- .github/workflows/notify.js
+++ .github/workflows/notify.js
@@ -5,12 +5,60 @@ const channelId = "code";
 const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
 const client = new WebClient(SLACK_API_TOKEN);
 
+const github = require('@actions/github');
+const context = github.context;
+
 async function sendSlackMessage() {
   try {
-    // Chama o método chat.postMessage usando o WebClient
-    const result = await client.chat.postMessage({
+    // const pullRequestData = {
+    //   id:context.payload.PULL_REQUEST_ID,
+    //   title:context.payload.PULL_REQUEST_TITLE,
+    //   url:context.payload.PULL_REQUEST_URL,
+    //   author:context.payload.PULL_REQUEST_AUTHOR,
+    //   state:context.payload.PULL_REQUEST_STATE,
+    //   repoName: context.repo.repo,
+    //   branchName:context.payload.pull_request.head.ref,
+    //   pullRequestUrl: context.payload.pull_request.html_url
+      
+    // };
+  
+    const pullRequestData = {
+      title: 'Fix issue with login',
+      pullRequestUrl: 'https://github.com/your-repo/pull/123',
+      user: 'john_doe',
+      branchName:"main",
+      state: 'opened',
+      repoName: 'your-repo'
+    };
+
+    const message ={
       channel: channelId,
-      text: "Teste"
+      blocks: [
+        {
+          type: 'section',
+          block_id: 'pr-section',
+          text: {
+            type: 'mrkdwn',
+            text: `New Pull Request on Project Youtube 🎥`
+          }
+        },
+        {
+          type: 'section',
+          block_id: 'pr-details',
+          text: {
+            type: 'mrkdwn',
+            text: `*Repository:* ${pullRequestData.repoName}\n*Tittle: ${pullRequestData.title}\n*Branch:* ${pullRequestData.branchName}\n*Author:* ${pullRequestData.author}\n*Pull Request URL:* ${pullRequestData.pullRequestUrl}`
+          }
+        },
+        {
+          type: 'divider'
+        }
+      ]
+    }
+
+    
+    const result = await client.chat.postMessage({
+      message
     });
 
     console.log(result);

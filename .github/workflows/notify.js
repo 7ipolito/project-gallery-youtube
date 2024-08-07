39c415c1e8af92ec932f156df29ccee3c958bdd2
Allan Hipólito <allanhipolito66@gmail.com>
2024-08-07 06:02:31 -0300

    teste

--- .github/workflows/notify.js
+++ .github/workflows/notify.js
@@ -10,27 +10,18 @@ const context = github.context;
 
 async function sendSlackMessage() {
   try {
-    // const pullRequestData = {
-    //   id:context.payload.PULL_REQUEST_ID,
-    //   title:context.payload.PULL_REQUEST_TITLE,
-    //   url:context.payload.PULL_REQUEST_URL,
-    //   author:context.payload.PULL_REQUEST_AUTHOR,
-    //   state:context.payload.PULL_REQUEST_STATE,
-    //   repoName: context.repo.repo,
-    //   branchName:context.payload.pull_request.head.ref,
-    //   pullRequestUrl: context.payload.pull_request.html_url
-      
-    // };
-  
     const pullRequestData = {
-      title: 'Fix issue with login',
-      pullRequestUrl: 'https://github.com/your-repo/pull/123',
-      user: 'john_doe',
-      branchName:"main",
-      state: 'opened',
-      repoName: 'your-repo'
+      id:context.payload.PULL_REQUEST_ID,
+      title:context.payload.PULL_REQUEST_TITLE,
+      url:context.payload.PULL_REQUEST_URL,
+      author:context.payload.PULL_REQUEST_AUTHOR,
+      state:context.payload.PULL_REQUEST_STATE,
+      repoName: context.repo.repo,
+      branchName:context.payload.pull_request.head.ref,
+      pullRequestUrl: context.payload.pull_request.html_url
+      
     };
-
+  
     const message ={
       channel: channelId,
       blocks: [
@@ -47,7 +38,7 @@ async function sendSlackMessage() {
           block_id: 'pr-details',
           text: {
             type: 'mrkdwn',
-            text: `*Repository:* ${pullRequestData.repoName}\n*Tittle: ${pullRequestData.title}\n*Branch:* ${pullRequestData.branchName}\n*Author:* ${pullRequestData.author}\n*Pull Request URL:* ${pullRequestData.pullRequestUrl}`
+            text: `*Repository:* ${pullRequestData.repoName}\n*Title*: ${pullRequestData.title}\n*Branch:* ${pullRequestData.branchName}\n*Author:* ${pullRequestData.author}\n*Pull Request URL:* ${pullRequestData.pullRequestUrl}`
           }
         },
         {

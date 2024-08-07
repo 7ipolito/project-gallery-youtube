862517ae2951c7d76f4e80c594eef2310846000c
Allan Hipólito <allanhipolito66@gmail.com>
2024-08-07 05:12:35 -0300

    teste

--- .github/workflows/notify.js
+++ .github/workflows/notify.js
@@ -1,17 +1,23 @@
-// ID of the channel you want to send the message to
+const { WebClient } = require('@slack/web-api');
+
+// ID do canal para onde você quer enviar a mensagem
 const channelId = "code";
- const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
+const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN;
+const client = new WebClient(SLACK_API_TOKEN);
 
-try {
-  // Call the chat.postMessage method using the WebClient
-  const result = await client.chat.postMessage({
-    channel: channelId,
-    token:SLACK_API_TOKEN,
-    text: "Teste"
-  });
+async function sendSlackMessage() {
+  try {
+    // Chama o método chat.postMessage usando o WebClient
+    const result = await client.chat.postMessage({
+      channel: channelId,
+      text: "Teste"
+    });
 
-  console.log(result);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
-catch (error) {
-  console.error(error);
-}
+
+// Chama a função assíncrona
+sendSlackMessage();
